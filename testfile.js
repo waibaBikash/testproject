fc8803a16ec547594e76dbaa9c3552ad21601947
waibaBikash <kavrelibikash@gmail.com>
2024-04-22 19:26:20 +1000

    simple js

--- testfile.js
+++ testfile.js
@@ -1,3 +1,8 @@
 function Test(){
   return "";
 }
+let name = prompt('your name');
+let firstName = name.slice(0,1);
+let firstNameUpper = firstName.toUpperCase();
+let finalName = firstName + firstNameUpper;
+console.log(finalName);
